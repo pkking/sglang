965e7f189bccafc4c87a661f2cad28dcf7db3470
mywaaagh_admin <pkwarcraft@gmail.com>
2025-07-09 23:03:21 +0800

    fix pip isntall
    Signed-off-by: mywaaagh_admin <pkwarcraft@gmail.com>

--- scripts/npu_ci_install_dependency.sh
+++ scripts/npu_ci_install_dependency.sh
@@ -1,12 +1,28 @@
 #!/bin/bash -e
 # Install the required dependencies in CI.
-
 pip install --upgrade pip
 
 pip config set global.index-url https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
 
 pip uninstall sgl-kernel -y || true
 
+# official PPA comes with ffmpeg 2.8, which lacks tons of features, we use ffmpeg 4.0 here
+add-apt-repository -y ppa:jonathonf/ffmpeg-4 # for ubuntu20.04 official PPA is already version 4.2, you may skip this step
+apt-get update -y
+apt-get install -y build-essential python3-dev python3-setuptools make cmake
+apt-get install -y ffmpeg libavcodec-dev libavfilter-dev libavformat-dev libavutil-dev
+# build decord
+git clone --recursive https://github.com/dmlc/decord
+cd decord
+mkdir build && cd build
+cmake .. -DUSE_CUDA=0 -DCMAKE_BUILD_TYPE=Release
+make
+cd ../python
+sed -i 's/maintainer_email.*/&\n    dependency_links=[\n        "https:\/\/mirrors.tuna.tsinghua.edu.cn\/pypi\/web\/simple"\n    ],/g' setup.py
+python3 setup.py install --user
+cd ../..
+pwd
+# note: make sure you have cmake 3.8 or later, you can install from cmake official website if it's too old
 
 pip install torch-npu==2.6.0rc1
 pip install triton-ascend==3.2.0rc2
