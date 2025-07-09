9d31e3f1611c38f2e2a679c8d3c16d1dba52e84a
mywaaagh_admin <pkwarcraft@gmail.com>
2025-07-09 21:47:11 +0800

    add ci for npu
    Signed-off-by: mywaaagh_admin <pkwarcraft@gmail.com>

---
+++ scripts/npu_ci_install_dependency.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+# Install the required dependencies in CI.
+
+pip install --upgrade pip
+
+pip config set global.index-url https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple
+
+pip uninstall sgl-kernel -y || true
+
+
+pip install torch-npu==2.6.0rc1
+pip install triton-ascend=3.2.0rc2
+
+pip install -e "python[all_npu]"
+
+pip install modelscope
+pip install pytest
