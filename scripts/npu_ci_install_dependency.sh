5239efb1903276309a3728cb277a17f2ac0f7a1d
Even Zhou <14368888+iforgetmyname@users.noreply.github.com>
2025-07-10 10:33:31 +0800

    upate npu_ci_install_dependency

--- scripts/npu_ci_install_dependency.sh
+++ scripts/npu_ci_install_dependency.sh
@@ -2,34 +2,62 @@
 # Install the required dependencies in CI.
 sed -i 's|ports.ubuntu.com|mirrors.tuna.tsinghua.edu.cn|g' /etc/apt/sources.list
 apt update -y
-apt install software-properties-common -y
-pip install --upgrade pip
+apt install -y build-essential cmake python3-pip python3-dev wget net-tools zlib1g-dev lld clang software-properties-common
+
 
 pip config set global.index-url https://mirrors.huaweicloud.com/repository/pypi/simple
-
+python3 -m pip install --upgrade pip
 pip uninstall sgl-kernel -y || true
 
+
+### Download MemFabricV2
+MEMFABRIC_URL="https://sglang-ascend.obs.cn-east-3.myhuaweicloud.com:443/sglang/mf_adapter-1.0.0-cp311-cp311-linux_aarch64.whl?AccessKeyId=HPUAXT4YM0U8JNTERLST&Expires=1783151861&Signature=3j10QDUjqk70enaq8lostYV2bEA%3D"
+wget -O mf_adapter.whl "${MEMFABRIC_URL}" && pip install ./mf_adapter.whl
+
+
+### Install vLLM
+VLLM_TAG=v0.8.5
+git clone --depth 1 https://github.com/vllm-project/vllm.git --branch $VLLM_TAG && \
+    cd vllm && VLLM_TARGET_DEVICE="empty" pip install -v -e . && cd ..
+
+
+### Install PyTorch and PTA
+PYTORCH_VERSION=2.6.0
+TORCHVISION_VERSION=0.21.0
+PTA_VERSION=2.6.0rc1
+pip install torch==$PYTORCH_VERSION torchvision==$TORCHVISION_VERSION --index-url https://download.pytorch.org/whl/cpu
+pip install torch_npu==$PTA_VERSION
+
+
+### Install Triton-Ascend
+TRITON_ASCEND_VERSION=3.2.0rc2
+pip install triton-ascend==$TRITON_ASCEND_VERSION
+
+
+pip install -e "python[srt_npu]"
+
+
 # official PPA comes with ffmpeg 2.8, which lacks tons of features, we use ffmpeg 4.0 here
-add-apt-repository -y ppa:jonathonf/ffmpeg-4 # for ubuntu20.04 official PPA is already version 4.2, you may skip this step
-apt-get update -y
-apt-get install -y build-essential python3-dev python3-setuptools make cmake
-apt-get install -y ffmpeg libavcodec-dev libavfilter-dev libavformat-dev libavutil-dev
-# build decord
-git clone --recursive https://github.com/dmlc/decord
-cd decord
-mkdir build && cd build
-cmake .. -DUSE_CUDA=0 -DCMAKE_BUILD_TYPE=Release
-make
-cd ../python
-sed -i 's/maintainer_email.*/&\n    dependency_links=[\n        "https:\/\/mirrors.tuna.tsinghua.edu.cn\/pypi\/web\/simple"\n    ],/g' setup.py
-python3 setup.py install --user
-cd ../..
-pwd
+# add-apt-repository -y ppa:jonathonf/ffmpeg-4 # for ubuntu20.04 official PPA is already version 4.2, you may skip this step
+# apt-get update -y
+# apt-get install -y build-essential python3-dev python3-setuptools make cmake
+# apt-get install -y ffmpeg libavcodec-dev libavfilter-dev libavformat-dev libavutil-dev
+# # build decord
+# git clone --recursive https://github.com/dmlc/decord
+# cd decord
+# mkdir build && cd build
+# cmake .. -DUSE_CUDA=0 -DCMAKE_BUILD_TYPE=Release
+# make
+# cd ../python
+# sed -i 's/maintainer_email.*/&\n    dependency_links=[\n        "https:\/\/mirrors.tuna.tsinghua.edu.cn\/pypi\/web\/simple"\n    ],/g' setup.py
+# python3 setup.py install --user
+# cd ../..
+# pwd
 # note: make sure you have cmake 3.8 or later, you can install from cmake official website if it's too old
 
-pip install torch-npu==2.7.1rc1 triton-ascend==3.2.0rc2 vllm==0.8.5
 
-pip install -e "python[all_npu]"
 
-pip install modelscope
-pip install pytest
+# pip install -e "python[all_npu]"
+
+# pip install modelscope
+# pip install pytest
