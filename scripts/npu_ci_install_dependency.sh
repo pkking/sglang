e2e324c483c9ba167a0e925a8ae164562b21e43f
Even Zhou <14368888+iforgetmyname@users.noreply.github.com>
2025-07-10 11:15:55 +0800

    fix triton dependency

--- scripts/npu_ci_install_dependency.sh
+++ scripts/npu_ci_install_dependency.sh
@@ -31,6 +31,7 @@ pip install torch_npu==$PTA_VERSION
 
 ### Install Triton-Ascend
 TRITON_ASCEND_VERSION=3.2.0rc2
+pip install attrs==24.2.0 numpy==1.26.4 scipy==1.13.1 decorator==5.1.1 psutil==6.0.0 pytest==8.3.2 pytest-xdist==3.6.1 pyyaml pybind11
 pip install triton-ascend==$TRITON_ASCEND_VERSION
 
 
